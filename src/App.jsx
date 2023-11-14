96debc1e0eb025e7dca1e0654fa865d79ef112ca
mmarentette <melrosemarentette@gmail.com>
2023-11-14 01:06:04 -0500

    Create Stocks route, nav link and component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Stocks from './Stocks';
 import './App.css';
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
       <nav>
         <Link to="/">iStocks</Link>
         <Link to="/about">About</Link>
+        <Link to="/stocks">Stocks</Link>
       </nav>
       <h1>iStocks</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/stocks" element={<Stocks />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </>
---
+++ src/Stocks.jsx
@@ -0,0 +1,5 @@
+export default function Stocks() {
+    return (
+        <h1>This is the Stocks component</h1>
+    )
+}
